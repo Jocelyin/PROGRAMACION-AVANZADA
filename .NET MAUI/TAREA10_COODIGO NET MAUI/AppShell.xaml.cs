cb99e3eebfba41ff055caaa836ba28426622a5e4
Jocelyin <jmateosaldierna@gmail.com>
2025-02-22 01:31:56 -0600

    Add files via upload

---
+++ .NET MAUI/TAREA10_COODIGO NET MAUI/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace TAREA_10_CODIGO_NET_MAUI
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
